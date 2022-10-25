aca2248835cc9b7241b900ea22ab341e8d36ab37
Asiyacm <111554799+Asiyacm@users.noreply.github.com>
2022-10-25 22:23:03 +0530

    Delete src directory

--- src/Temperature.java
+++
@@ -1,7 +0,0 @@
-public class Temperature {
-    public static void main(String[] args) {
-        int Fahrenheit = 176, Celsius = 0;
-        Celsius = ((Fahrenheit - 32) * 5) / 9;
-        System.out.println("celsius value is: " + Celsius);
-    }
-}
