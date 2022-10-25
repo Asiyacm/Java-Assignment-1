4658e9442b4fcfb361fe8a71253ec9e087958a02
Asiyacm <asiyacmuhammed@gmail.com>
2022-10-25 22:16:17 +0530

    second commit

---
+++ src/Trianglepattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Trianglepattern {
+    public static void main(String[] args)
+
+    {
+        int i,j,n;
+
+        System.out.print("Input number n is : ");
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++) {
+
+                System.out.print(i);
+            }
+            System.out.println("");
+        }
+    }
+}
